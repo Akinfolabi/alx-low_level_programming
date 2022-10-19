eae5a37063eb76979ef620b4c34023ccfb2ea071
Akinfolabi <akinfolabi1120@gmail.com>
2022-10-19 21:23:45 +0100

    0-putchar 1c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,6 +10,14 @@
 */
 
 int main(void){
-putchar("_putchar\n");
+int str[] = {95, 70, 75, 74, 63, 68, 61, 72};
+int count, sz;
+
+sz = sizeof(str) / sizeof(int);
+for (count = 0; count < sz; count++)
+{
+_putchar(str[count]);
+}
+_putchar('\n');
 return (0);
 }
